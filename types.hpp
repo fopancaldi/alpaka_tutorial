c7e347cf2944ee1e090a0e98b529c496c3e27b07
Francesco Orso Pancaldi <105540514+fopancaldi@users.noreply.github.com>
2025-11-14 16:42:03 +0100

    Add aliases for 2D buffers

--- types.hpp
+++ types.hpp
@@ -1,9 +1,7 @@
 #pragma once
 
 #include "alpaka/alpaka.hpp"
-#include <cassert>
 #include <cstdint>
-#include <type_traits>
 
 namespace alpaka_tutorial {
 
@@ -15,6 +13,11 @@ using Dim1 = a::DimInt<1>;
 using Dim2 = a::DimInt<2>;
 using Idx = uint32_t;
 
+template <typename TDim>
+using Vec = a::Vec<TDim, Idx>;
+using Vec1 = Vec<Dim1>;
+using Vec2 = Vec<Dim2>;
+
 using PlatformH = a::PlatformCpu;
 using DevH = a::DevCpu;
 using QueueH = a::Queue<DevH, a::Blocking>;
@@ -23,11 +26,15 @@ template <typename TElem, typename TDim>
 using BufH = a::Buf<DevH, TElem, TDim, Idx>;
 template <typename TElem>
 using Buf1H = BufH<TElem, Dim1>;
+template <typename TElem>
+using Buf2H = BufH<TElem, Dim2>;
 
 template <typename TElem, typename TDim>
 using ViewH = a::ViewPlainPtr<DevH, TElem, TDim, Idx>;
 template <typename TElem>
 using View1H = ViewH<TElem, Dim1>;
+template <typename TElem>
+using View2H = ViewH<TElem, Dim2>;
 
 #if defined ALPAKA_ACC_CPU_B_SEQ_T_SEQ_ENABLED
 using Platform = a::PlatformCpu;
@@ -58,11 +65,15 @@ template <typename TElem, typename TDim>
 using Buf = a::Buf<Device, TElem, TDim, Idx>;
 template <typename TElem>
 using Buf1 = Buf<TElem, Dim1>;
+template <typename TElem>
+using Buf2 = Buf<TElem, Dim2>;
 
 template <typename TElem, typename TDim>
 using View = a::ViewPlainPtr<Device, TElem, TDim, Idx>;
 template <typename TElem>
 using View1 = View<TElem, Dim1>;
+template <typename TElem>
+using View2 = View<TElem, Dim2>;
 
 using Acc1 = Acc<Dim1>;
 
