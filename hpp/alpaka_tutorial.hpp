867adc89f999895c4c4d980aae9cbe4128d05085
Francesco Orso Pancaldi <105540514+fopancaldi@users.noreply.github.com>
2025-12-14 09:51:51 +0100

    Add TODO

--- hpp/alpaka_tutorial.hpp
+++ hpp/alpaka_tutorial.hpp
@@ -4,6 +4,7 @@
 #include "concepts.hpp"
 #include "config.hpp"
 #include "constants.hpp"
+#include "device.hpp"
 #include "typedefs.hpp"
 #include "util.hpp"
 #include "work_division.hpp"
---
+++ hpp/device.hpp
@@ -0,0 +1,3 @@
+#pragma once
+
+// TODO: Add functions to get devices, etc.
