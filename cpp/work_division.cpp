0a07b869855b382c52166109f11dda1b6cc03a93
Francesco Orso Pancaldi <105540514+fopancaldi@users.noreply.github.com>
2025-12-17 20:05:11 +0100

    Add basic example of valid and invalid work divisions

---
+++ cpp/work_division.cpp
@@ -0,0 +1,60 @@
+#include "alpaka_tutorial.hpp"
+
+#include <cassert>
+#include <stdexcept>
+
+#define AT_CHECK_THROWS(cmd)                                                                       \
+    do {                                                                                           \
+        bool hasThrown = false;                                                                    \
+        try {                                                                                      \
+            cmd;                                                                                   \
+        } catch (std::runtime_error) {                                                             \
+            hasThrown = true;                                                                      \
+        }                                                                                          \
+        assert(hasThrown /**/&& #cmd && "did not throw");                                          \
+    } while (false);
+
+namespace a = alpaka;
+namespace at = alpaka_tutorial;
+
+struct Kernel {
+    template <at::concepts::Acc TAcc>
+    ALPAKA_FN_ACC void operator()(TAcc const&) const {}
+};
+
+template <at::concepts::Dim TDim>
+void RunKernel(at::Queue& queue, at::Vec<TDim> blocks, at::Vec<TDim> blockThreads,
+               at::Vec<TDim> threadElements) {
+    at::WorkDiv<TDim> const workDiv(blocks, blockThreads, threadElements);
+    a::exec<at::Acc<TDim>>(queue, workDiv, Kernel{});
+    a::wait(queue);
+}
+
+int main() {
+    using namespace at;
+
+    Platform platform;
+    assert(a::getDevCount(platform) > 0);
+    Device device = a::getDevByIdx(platform, 0);
+    Queue queue(device);
+
+    // Single thread
+#ifndef ALPAKA_ACC_GPU_CUDA_ENABLED
+    RunKernel<Dim1D>(queue, 1, 1, 1);
+    if constexpr (a::isSingleThreadAcc<Acc1D>) {
+        AT_CHECK_THROWS(RunKernel<Dim1D>(queue, 1, 2, 1))
+    } else {
+        RunKernel<Dim1D>(queue, 1, 2, 1);
+    }
+#else
+    AT_CHECK_THROWS(RunKernel<Dim1D>(queue, 1, 1025, 1))
+#endif
+
+    // Max grid size
+    RunKernel<Dim2D>(queue, {1, 1 << 16}, {1, 1}, {1, 1});
+#ifndef ALPAKA_ACC_GPU_CUDA_ENABLED
+    RunKernel<Dim2D>(queue, {1 << 16, 1}, {1, 1}, {1, 1});
+#else
+    AT_CHECK_THROWS(RunKernel<Dim2D>(queue, {1 << 16, 1}, {1, 1}, {1, 1}))
+#endif
+}
